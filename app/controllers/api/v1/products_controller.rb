baa76b89ecb773da33070219eb5543ee69a35672
Pierre FILSTROFF <pierre.filstroff@gmail.com>
2021-11-20 21:00:08 +0100

    Added the update method in the product controller + test

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ProductsController < ApplicationController
-  before_action :set_product, only: %i[show]
+  before_action :set_product, only: %i[show update]
 
   # GET /api/v1/products
   def index
@@ -12,10 +12,26 @@ class Api::V1::ProductsController < ApplicationController
     render json: @product
   end
 
+  # PATCH/PUT /api/v1/products/1
+  def update
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_product
     @product = Product.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def product_params
+    params.require(:product).permit(
+      :price_cents
+    )
   end
 end
